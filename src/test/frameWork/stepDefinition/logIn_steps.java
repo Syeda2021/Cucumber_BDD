152167d4c366db4d9e937a32c84f75af6c9ef548
Syeda2021 <jahansyeda29@gmail.com>
2021-10-08 22:42:13 -0400

    FrameWork

---
+++ src/test/frameWork/stepDefinition/logIn_steps.java
@@ -0,0 +1,26 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class logIn_steps {
+    @Given("I am at TalentTEK Homepage")
+    public void iAmAtTalentTEKHomepage() {
+        
+    }
+
+    @And("I enter a valid email address")
+    public void iEnterAValidEmailAddress() {
+        
+    }
+
+    @And("I enter a valid password")
+    public void iEnterAValidPassword() {
+        
+    }
+
+    @When("I click on Login Button")
+    public void iClickOnLoginButton() {
+    }
+}
